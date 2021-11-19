c279e427b7c2a4384dbc4e1fb82cf48b01eb9026
showierdata 9978 <68120127+showierdata9978@users.noreply.github.com>
2021-11-19 14:40:56 +0000

    auto formatted everything

--- functions/Bank.py
+++ functions/Bank.py
@@ -1,17 +1,14 @@
- 
-def bank(HashedPas,username,JustCreatedAccount,Bank):
-      import hashlib
-      message = hashlib.sha256()
-      from replit import db
-      if HashedPas == db[username]:
-       if JustCreatedAccount :
-        AmmountOfIOU = 200
-        JustCreatedAccount = False 
-       bankhash = Bank + username + HashedPas
-       bankhash = message.update(bankhash.encode())
-       db[bankhash] = AmmountOfIOU
-       print(AmmountOfIOU)
-      else:
-         print("wrong username or pasword") 
-       
-     
+def bank(HashedPas, username, JustCreatedAccount, Bank):
+    import hashlib
+    message = hashlib.sha256()
+    from replit import db
+    if HashedPas == db[username]:
+        if JustCreatedAccount:
+            AmmountOfIOU = 200
+            JustCreatedAccount = False
+        bankhash = Bank + username + HashedPas
+        bankhash = message.update(bankhash.encode())
+        db[bankhash] = AmmountOfIOU
+        print(AmmountOfIOU)
+    else:
+        print("wrong username or pasword")
--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-
 if __name__ == '__main__':
- #functions
- from functions.SignIn_signup import signinpromt
- from functions.Bank import bank
- signinpromt()
- from functions.SignIn_signup import hashedpas , username , JustCreatedAccount , Bank
- bank(hashedpas,username,JustCreatedAccount,Bank)
-
-
-
- 
-
-
-
-
-
-    
- 
+    #functions
+    from functions.SignIn_signup import signinpromt
+    from functions.Bank import bank
+    signinpromt()
+    from functions.SignIn_signup import hashedpas, username, JustCreatedAccount, Bank
+    bank(hashedpas, username, JustCreatedAccount, Bank)
